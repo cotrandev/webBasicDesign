91ae7d6732e3ace4f1fcb18f1b250dd2690fd479
cotrandev <yd19849@gmail.com>
2023-11-03 13:42:01 +0700

    update_se1

--- data/listProduct.js
+++ data/listProduct.js
@@ -2,71 +2,71 @@ export {listProduct}
 const listProduct = [
     {
         id : 1,
-        url : "../assets/img/listProduct/product_1.webp",   
+        url : "../assets/img/listProduct/product_11.webp",   
         name : "áo thun basic V details",
         price: "10$"
 
     },
     {
         id : 2,
-        url : "../assets/img/listProduct/product_2.webp",   
+        url : "../assets/img/listProduct/product_12.webp",   
         name : "Áo Polo Wool Ziczac Waves",
         price: "12$"
 
     },
     {
         id : 3,
-        url : "../assets/img/listProduct/product_3.jpeg",   
-        name : "Áo Polo Nerdy Brown Sleeves",
+        url : "../assets/img/listProduct/product_13.jpeg",   
+        name : "Áo Thun Wash Skateboard Art",
         price: "12$"
 
     },
     {
         id : 4,
-        url : "../assets/img/listProduct/product_4.webp",   
-        name : "Quần Tây Single Linen Base",
+        url : "../assets/img/listProduct/product_14.webp",   
+        name : "Áo Polo Nerdy Brown Sleeves",
         price: "15$"
 
     },
     {
         id : 5,
-        url : "../assets/img/listProduct/product_5.webp",   
+        url : "../assets/img/listProduct/product_15.webp",   
         name : "Quần Dài Orange Label Slim",
         price: "14$"
 
     },
     {
         id : 6,
-        url : "../assets/img/listProduct/product_6.webp",   
+        url : "../assets/img/listProduct/product_16.webp",   
         name : "Áo Thun Boxy Hugging Beige",
         price: "10$"
 
     },
     {
         id : 7,
-        url : "../assets/img/listProduct/product_7.webp",   
+        url : "../assets/img/listProduct/product_17.webp",   
         name : "Quần Jean Skinny White Label Ironic",
         price: "20$"
 
     },
     {
         id : 8,
-        url : "../assets/img/listProduct/product_8.webp",   
+        url : "../assets/img/listProduct/product_18.webp",   
         name : "Áo Thun Lolipop White 360GSM",
         price: "13$"
 
     },
     {
         id : 9,
-        url : "../assets/img/listProduct/product_9.webp",   
-        name : "Giày Loafer Classic Frontape Creamy",
+        url : "../assets/img/listProduct/img_1.webp",   
+        name : "Áo Thun Boxy Over Black White Art",
         price: "25$"
 
     },
     {
         id : 10,
-        url : "../assets/img/listProduct/product_10.webp",   
-        name : "Ví Abstract Diamond Edge",
+        url : "../assets/img/listProduct/img_2.webp",   
+        name : "Áo Thun Special Nametag 360GSM",
         price: "30$"
 
     }
